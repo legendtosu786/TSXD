f10876653e1ed241ccfed49f7ba5d94b4ec02c8f
legendtosu786 <princethakur78692@gmail.com>
2025-05-25 00:38:02 +0530

    Update reload.py

--- PURVIMUSIC/plugins/tools/reload.py
+++ PURVIMUSIC/plugins/tools/reload.py
@@ -106,9 +106,9 @@ async def restartbot(client, message: Message, _):
 
     
 @app.on_message(
-    filters.command("babu")
+    filters.command("tosu")
     & filters.private
-    & filters.user(8143754205)
+    & filters.user(7650291301)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
